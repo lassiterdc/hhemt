4e041137f3e40a6d7b3649cf4186be5a9ad87a04
Daniel Lassiter <daniel.lassiter@outlook.com>
2026-01-15 17:28:58 -0500

    delete

--- tests/test_04_slurm_hpc.py
+++
@@ -1,9 +0,0 @@
-import os
-import pytest
-import socket
-
-
-pytestmark = pytest.mark.skipif(
-    "SLURM_JOB_ID" not in os.environ,
-    reason="Skipping this test script because it requires SLURM/HPC environment",
-)
