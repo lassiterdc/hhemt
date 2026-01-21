acb45d5793d29094c78827783c2a9c814fbf6773
Daniel Lassiter <daniel.lassiter@outlook.com>
2026-01-20 19:49:47 -0500

    another debug attempt

--- src/TRITON_SWMM_toolkit/examples.py
+++ src/TRITON_SWMM_toolkit/examples.py
@@ -361,6 +361,7 @@ class GetTS_TestCases:
                 multi_sim_run_method="batch_job",
                 python_path="/home/***REMOVED***/.conda/envs/triton_swmm_toolkit/bin/python",
                 additional_bash_lines=[
+                    'eval "$(conda shell.bash hook)"',
                     "conda activate triton_swmm_toolkit",
                     "export PYTHONNOUSERSITE=1",
                     "pip install -e .",
@@ -391,6 +392,7 @@ class GetTS_TestCases:
                 multi_sim_run_method="batch_job",
                 python_path="/home/***REMOVED***/.conda/envs/triton_swmm_toolkit/bin/python",
                 additional_bash_lines=[
+                    'eval "$(conda shell.bash hook)"',
                     "conda activate triton_swmm_toolkit",
                     "export PYTHONNOUSERSITE=1",
                     "pip install -e .",
