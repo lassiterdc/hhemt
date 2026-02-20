7461a032a619d05a8be0064195db0d78382ba5ea
Daniel Lassiter <daniel.lassiter@outlook.com>
2026-02-20 14:51:13 -0500

    updating uva sensitivity test cases for more targeted evaluation

--- tests/fixtures/test_case_catalog.py
+++ tests/fixtures/test_case_catalog.py
@@ -131,7 +131,12 @@ class GetTS_TestCases:
 
 class UVA_TestCases:
     sensitivity_UVA_cpu_minimal = "benchmarking_uva_minimal.xlsx"
-    sensitivity_analysis_uva_suite = "full_benchmarking_experiment_uva_test.xlsx"
+    sensitivity_analysis_uva_suite_cpu = (
+        "full_benchmarking_experiment_uva_test_cpu.xlsx"
+    )
+    sensitivity_analysis_uva_suite_gpu = (
+        "full_benchmarking_experiment_uva_test_gpu.xlsx"
+    )
 
     @classmethod
     def retrieve_norfolk_UVA_multisim_1cpu_case(
@@ -188,15 +193,58 @@ class UVA_TestCases:
         )
 
     @classmethod
-    def benchmarking_norfolk_irene(
+    def benchmarking_norfolk_irene_cpu(
         cls, start_from_scratch: bool = False, download_if_exists: bool = False
     ):
         """UVA HPC sensitivity analysis."""
         # example_name = "norfolk_irene"
-        analysis_name = "test_uva_sensitivity_suite"
+        analysis_name = "test_uva_sensitivity_suite_cpu"
         sensitivity = (
             all_examples.ex_Nrflk().test_case_directory
-            / cls.sensitivity_analysis_uva_suite
+            / cls.sensitivity_analysis_uva_suite_cpu
+        )
+
+        analysis_overrides = {
+            "toggle_sensitivity_analysis": True,
+            "sensitivity_analysis": sensitivity,
+            "run_mode": "serial",
+            "hpc_time_min_per_sim": 40,
+            "n_mpi_procs": 1,
+            "n_omp_threads": 1,
+            "n_nodes": 1,
+            "n_gpus": 0,
+            "mem_gb_per_cpu": 2,
+            "hpc_max_simultaneous_sims": 100,
+            "hpc_total_job_duration_min": 60 * 72,
+        }
+
+        system_overrides = {
+            "toggle_triton_model": False,
+            "toggle_tritonswmm_model": True,
+            "toggle_swmm_model": False,
+            "gpu_compilation_backend": "CUDA",
+        }
+
+        return GetTS_TestCases._retrieve_norfolk_case(
+            analysis_name=analysis_name,
+            start_from_scratch=start_from_scratch,
+            download_if_exists=download_if_exists,
+            n_events=1,
+            platform_config=cnst.UVA_DEFAULT_PLATFORM_CONFIG,
+            analysis_overrides=analysis_overrides,
+            system_overrides=system_overrides,
+        )
+
+    @classmethod
+    def benchmarking_norfolk_irene_gpu(
+        cls, start_from_scratch: bool = False, download_if_exists: bool = False
+    ):
+        """UVA HPC sensitivity analysis."""
+        # example_name = "norfolk_irene"
+        analysis_name = "test_uva_sensitivity_suite_gpu"
+        sensitivity = (
+            all_examples.ex_Nrflk().test_case_directory
+            / cls.sensitivity_analysis_uva_suite_gpu
         )
 
         analysis_overrides = {
